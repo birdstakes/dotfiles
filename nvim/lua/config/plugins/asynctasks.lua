e7b5641cf7e06059e918ab2caac32a04d8ee8682
Josh Steffen <josh0x53@gmail.com>
2021-12-16 15:10:44 -0500

    nvim: Use default asynctasks config name

--- nvim/lua/config/plugins/asynctasks.lua
+++ nvim/lua/config/plugins/asynctasks.lua
@@ -1,5 +1,4 @@
 vim.g.asyncrun_open = 15
 vim.g.asynctasks_term_pos = 'bottom'
 vim.g.asynctasks_term_rows = 15
-vim.g.asynctasks_config_name = { '.tasks/tasks.ini', '.tasks' }
 vim.g.asynctasks_template = vim.fn.stdpath('config') .. '/task_template.ini'
