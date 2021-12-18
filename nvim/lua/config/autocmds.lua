0e0885ec73f77599a29b38548064a556b55532c7
Josh Steffen <josh0x53@gmail.com>
2021-12-18 11:33:20 -0500

    nvim: Set nobuflisted on quickfix window

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -5,4 +5,5 @@ local config_pattern = vim.fn.stdpath('config'):gsub('\\', '/') .. '/*.lua'
 util.augroup('config', {
     { 'BufWritePost', config_pattern, 'lua require("config.util").reload()' },
     { 'FileType', 'help,lspinfo,man,qf', 'nnoremap <silent> <buffer> q :close<cr>' },
+    { 'FileType', 'qf', 'set nobuflisted' },
 })
