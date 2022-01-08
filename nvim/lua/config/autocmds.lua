cf23657175a0aa3c24445d3860f6d9d6108a65b6
Josh Steffen <josh0x53@gmail.com>
2022-01-07 21:06:17 -0500

    nvim: Always open quickfix window below everything else

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,4 +6,5 @@ util.augroup('config', {
     { 'BufWritePost', config_pattern, 'lua require("config.util").reload()' },
     { 'FileType', 'help,lspinfo,man,qf', 'nnoremap <silent> <buffer> q :close<cr>' },
     { 'FileType', 'qf', 'set nobuflisted' },
+    { 'FileType', 'qf', 'wincmd J' },
 })
